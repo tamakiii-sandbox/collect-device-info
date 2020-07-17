5a514a95fae4687b9cb4e4716573fa6e2547f279
tamakiii <tamakiii@users.noreply.github.com>
2020-07-18 04:00:40 +0900

    fix

--- src/docomo.js
+++ src/docomo.js
@@ -22,12 +22,11 @@ const puppeteer = require('puppeteer-core');
       }));
   });
 
-  // const results = await Promise.all([links[0]].map(async link => {
   const results = await Promise.all(links.map(async link => {
     await page.goto(link.href);
     return await page.evaluate(() => ({
       name: document.querySelector('#spec h1').innerText,
-      data: Array.from(document.querySelectorAll(`#spec > div > div > div > .detail[label]`)).map((detail) => {
+      data: Array.from(document.querySelectorAll(`#spec > div > div > div > .detail[label]`)).map(detail => {
         const h3s = detail.querySelectorAll('h3');
         const tables = detail.querySelectorAll('div.spec.mb20 table');
         return {
